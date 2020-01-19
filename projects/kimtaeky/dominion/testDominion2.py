518c3f7e08b126e6085ca93c2d978545a8541fb0
tkim949 <tkim949@gmail.com>
2020-01-19 09:28:30 -0800

    Add more refactoring and tweak two testDominion cards

--- projects/kimtaeky/dominion/testDominion2.py
+++ projects/kimtaeky/dominion/testDominion2.py
@@ -19,44 +19,9 @@ if len(player_names)>2:
 else:
     nV=8
 nC = -10 + 10 * len(player_names)
-
+#refactor to testUtility
 box = testUtility.GetBoxes(nV)
-#Define box
-
-"""
-box = {}
-box["Woodcutter"]=[Dominion.Woodcutter()]*10
-box["Smithy"]=[Dominion.Smithy()]*10
-box["Laboratory"]=[Dominion.Laboratory()]*10
-box["Village"]=[Dominion.Village()]*10
-box["Festival"]=[Dominion.Festival()]*10
-box["Market"]=[Dominion.Market()]*10
-box["Chancellor"]=[Dominion.Chancellor()]*10
-box["Workshop"]=[Dominion.Workshop()]*10
-box["Moneylender"]=[Dominion.Moneylender()]*10
-box["Chapel"]=[Dominion.Chapel()]*10
-box["Cellar"]=[Dominion.Cellar()]*10
-box["Remodel"]=[Dominion.Remodel()]*10
-box["Adventurer"]=[Dominion.Adventurer()]*10
-box["Feast"]=[Dominion.Feast()]*10
-box["Mine"]=[Dominion.Mine()]*10
-box["Library"]=[Dominion.Library()]*10
-box["Gardens"]=[Dominion.Gardens()]*nV
-box["Moat"]=[Dominion.Moat()]*10
-box["Council Room"]=[Dominion.Council_Room()]*10
-box["Witch"]=[Dominion.Witch()]*10
-box["Bureaucrat"]=[Dominion.Bureaucrat()]*10
-box["Militia"]=[Dominion.Militia()]*10
-box["Spy"]=[Dominion.Spy()]*10
-box["Thief"]=[Dominion.Thief()]*10
-box["Throne Room"]=[Dominion.Throne_Room()]*10
-"""
-
-supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
-                3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
-                4:['Gardens','Bureaucrat','Feast','Militia','Moneylender','Remodel','Smithy','Spy','Thief','Throne Room'],
-                5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
-                6:['Gold','Adventurer'],8:['Province']}
+supply_order = testUtility.GetSupplyOrder()
 
 #Pick 10 cards from box to be in the supply.
 boxlist = [k for k in box]
@@ -64,28 +29,19 @@ random.shuffle(boxlist)
 random10 = boxlist[:10]
 supply = defaultdict(list,[(k,box[k]) for k in random10])
 
-
 #The supply always has these cards
-supply["Copper"]=[Dominion.Copper()]*(60-len(player_names)*7)
-supply["Silver"]=[Dominion.Silver()]*40
-supply["Gold"]=[Dominion.Gold()]*30
-supply["Estate"]=[Dominion.Estate()]*nV
-supply["Duchy"]=[Dominion.Duchy()]*nV
-supply["Province"]=[Dominion.Province()]*nV
-supply["Curse"]=[Dominion.Curse()]*nC
+# refactoring this
+# change nV to 1 which was supposed to be 12 when player is 3 or more to make a bug
+supply = testUtility.GetSupply(1, nC, supply, player_names)
 
 #initialize the trash
 trash = []
 
 #Costruct the Player objects
 players = []
-for name in player_names:
-    if name[0]=="*":
-        players.append(Dominion.ComputerPlayer(name[1:]))
-    elif name[0]=="^":
-        players.append(Dominion.TablePlayer(name[1:]))
-    else:
-        players.append(Dominion.Player(name))
+#refactoring
+players = testUtility.GetPlayers(players, player_names)
+
 
 #Play the game
 turn  = 0
@@ -105,7 +61,6 @@ while not Dominion.gameover(supply):
         if not Dominion.gameover(supply):
             print("\r")
             player.turn(players,supply,trash)
-            
 
 #Final score
 dcs=Dominion.cardsummaries(players)
--- projects/kimtaeky/dominion/testUtility.py
+++ projects/kimtaeky/dominion/testUtility.py
@@ -38,3 +38,36 @@ def GetBoxes(nV):
     box["Thief"]=[Dominion.Thief()]*10
     box["Throne Room"]=[Dominion.Throne_Room()]*10
     return box
+
+
+def GetSupplyOrder():
+
+    supply_order = {0: ['Curse', 'Copper'], 2: ['Estate', 'Cellar', 'Chapel', 'Moat'],
+                    3: ['Silver', 'Chancellor', 'Village', 'Woodcutter', 'Workshop'],
+                    4: ['Gardens', 'Bureaucrat', 'Feast', 'Militia', 'Moneylender', 'Remodel', 'Smithy', 'Spy', 'Thief',
+                        'Throne Room'],
+                    5: ['Duchy', 'Market', 'Council Room', 'Festival', 'Laboratory', 'Library', 'Mine', 'Witch'],
+                    6: ['Gold', 'Adventurer'], 8: ['Province']}
+    return supply_order
+
+
+def GetSupply(nV, nC, supply, player_names):
+    supply["Copper"] = [Dominion.Copper()] * (60 - len(player_names) * 7)
+    supply["Silver"] = [Dominion.Silver()] * 40
+    supply["Gold"] = [Dominion.Gold()] * 30
+    supply["Estate"] = [Dominion.Estate()] * nV
+    supply["Duchy"] = [Dominion.Duchy()] * nV
+    supply["Province"] = [Dominion.Province()] * nV
+    supply["Curse"] = [Dominion.Curse()] * nC
+    return supply
+
+
+def GetPlayers(players, player_names):
+    for name in player_names:
+        if name[0] == "*":
+            players.append(Dominion.ComputerPlayer(name[1:]))
+        elif name[0] == "^":
+            players.append(Dominion.TablePlayer(name[1:]))
+        else:
+            players.append(Dominion.Player(name))
+    return players
